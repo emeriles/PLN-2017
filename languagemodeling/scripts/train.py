4bd1f765632636cb16145e5a6230ce02c9caabd7
Emanuel Meriles <emeriles@gmail.com>
2017-04-13 22:31:30 -0300

    agregado train.py con opciones de entrenar ngram o addone

--- languagemodeling/scripts/train.py
+++ languagemodeling/scripts/train.py
@@ -1,40 +1,29 @@
 """Train an n-gram model.
 
 Usage:
-  train.py -n <n> -o <file>
+  train.py -n <n> [-m <model>] -o <file>
   train.py -h | --help
 
 Options:
   -n <n>        Order of the model.
+  -m <model>    Model to use [default: ngram]:
+                  ngram: Unsmoothed n-grams.
+                  addone: N-grams with add-one smoothing.
   -o <file>     Output model file.
   -h --help     Show this screen.
 """
 from docopt import docopt
 import pickle
+from sys import exit
 
-#from nltk.corpus import gutenberg
 from nltk.corpus import PlaintextCorpusReader
 from nltk.tokenize import RegexpTokenizer
 
-from languagemodeling.ngram import NGram
+from languagemodeling.ngram import NGram, AddOneNGram
 
 
-if __name__ == '__main__':
-    """opts = docopt(__doc__)
 
-    # load the data
-    sents = gutenberg.sents('austen-emma.txt')
-
-    # train the model
-    n = int(opts['-n'])
-    model = NGram(n, sents)
-
-    # save it
-    filename = opts['-o']
-    f = open(filename, 'wb')
-    pickle.dump(model, f)
-    f.close()"""
-
+def _get_sents(filename):
 
     pattern = r'''(?ix)    # set flag to allow verbose regexps
           (?:sr\.|sra\.)
@@ -47,6 +36,26 @@ if __name__ == '__main__':
 
     tokenizer = RegexpTokenizer(pattern)
 
-    corpus = PlaintextCorpusReader('.', 'continuidaddelosparques.txt', word_tokenizer=tokenizer)
-    for sent in corpus.sents():
-        print (sent)
+    corpus = PlaintextCorpusReader('.', filename, word_tokenizer=tokenizer)
+    return corpus.sents()
+
+if __name__ == '__main__':
+    opts = docopt(__doc__)
+
+    #get parameters and check them
+    model_n = int(opts['-n'])
+    filename = opts['-o']
+    try:
+        model_t = opts['-m']
+    except:
+        model_t = 'ngram'
+    if (model_t not in {'ngram', 'addone'}):
+        print ('Wrong -m parameter. Expected \'ngram\' or \' addone\'')
+        exit()
+
+    # train the model in the chosen mode
+    sents = _get_sents(filename)        #  TO DO: alert when file not found
+    if (model_t == 'ngram'):
+        ngram = NGram(model_n, sents)
+    else:
+        ngram = AddOneNGram(model_n, sents)
